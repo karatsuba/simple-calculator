a69a5405c95e0f7a6450284b597d3ed54bf1d1be
karatsuba <romanchuhan18@gmail.com>
2018-07-13 16:08:18 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # simple-calculator
 Simple Java Calculator
+
+## Build
+
+gradle build
+
+## Test
+
+gradle test
+
+## Usage from command line
+
+go to `build/classes/java/main`
+run `java Calculator ADD 2 2`
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,10 @@
+import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-public class CalculatorTest {
+@RunWith(JUnit4.class)
+public class CalculatorTest extends TestCase {
 
     @Test
     public void testCalculatorADDWithValidParams() throws Exception{
